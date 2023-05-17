4930c92e95ad319eb14b22a3275e32a43e3eb5b8
Steve Grard <grard.steve@gmail.com>
2023-05-17 12:23:13 +0200

    Add of a loader

--- assets/js/modules/api-access.js
+++ assets/js/modules/api-access.js
@@ -1,13 +1,31 @@
 import { showInformations } from "./showInformations.js"
 
 export function apiAccess(){
+    displayLoading()
     fetch("https://thatsthespir.it/api")
         .then((response) => response.text())
         .then((text) => {
             let quote = JSON.parse(text)
+            hideLoading()
             showInformations(quote)
+            
         })
         .catch((error) => {
             console.log("Terre was an error!", error)
         })
+}
+
+export function apiAgify(){
+    // const fetchName = 
+    fetch("https://api.agify.io/?name=")
+}
+
+function displayLoading(){
+    let loader = document.getElementById("loader")
+    loader.classList.add("display-inline-block")
+}
+
+function hideLoading(){
+    let loader = document.getElementById("loader")
+    loader.classList.remove("display-inline-block")
 }
--- assets/js/modules/showInformations.js
+++ assets/js/modules/showInformations.js
@@ -3,6 +3,9 @@ const quote = document.getElementById("quote")
 
 export function showInformations(informations){
     console.log(informations)
+    if(informations.photo == ""){
+        informations.photo="https://m.media-amazon.com/images/I/61xNq7dsmlL._AC_UF894,1000_QL80_.jpg"
+    }
     character.style.backgroundImage = `url(${informations.photo})`
     quote.innerHTML = `
         <figure>
